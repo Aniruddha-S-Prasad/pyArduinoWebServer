b8f2d6c86ba1c5c676174fcfad5a890a1824d474
Aniruddha-S-Prasad <aniruddha.asp12@gmail.com>
2021-11-07 14:51:02 +0100

    Added port and baudrate selection and moved index to templates

--- readArduinoSerial.py
+++ readArduinoSerial.py
@@ -7,73 +7,76 @@ import serial
 
 
 class SensorData:
-    def __str__(self) -> str:
-        return 'Temperature: ' + str(self.temperature) + ' Humidity: ' + str(self.humidity)
+	def __str__(self) -> str:
+		return 'Temperature: ' + str(self.temperature) + ' Humidity: ' + str(self.humidity)
 
-    def __init__(self, temperature=0.0, humidity=0.0):
-        self.temperature = temperature
-        self.humidity = humidity
-
-
-def listSerialPorts():
-    """ Lists serial port names
-
-        :raises EnvironmentError:
-            On unsupported or unknown platforms
-        :returns:
-            A list of the serial ports available on the system
-    """
-    if sys.platform.startswith('win'):
-        ports = ['COM%s' % (i + 1) for i in range(256)]
-    elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
-        # this excludes your current terminal "/dev/tty"
-        ports = glob.glob('/dev/tty[A-Za-z]*')
-    elif sys.platform.startswith('darwin'):
-        ports = glob.glob('/dev/tty.*')
-    else:
-        raise EnvironmentError('Unsupported platform')
-
-    result = []
-    for port in ports:
-        try:
-            s = serial.Serial(port)
-            s.close()
-            result.append(port)
-        except (OSError, serial.SerialException):
-            pass
-    return result
+	def __init__(self, temperature=0.0, humidity=0.0):
+		self.temperature = temperature
+		self.humidity = humidity
 
 
-def listenSensorData(sensorObj):
-    serialPorts = listSerialPorts()
-    if len(serialPorts) == 0:
-        sys.exit('No Serial Ports found. Exiting')
+def list_serial_ports():
+	""" Lists serial port names
 
-    print(serialPorts)
-    arduinoSerialDevice = serial.Serial(serialPorts[0], 115200)
-    arduinoString = 'Error, No Data'
+		:raises EnvironmentError:
+			On unsupported or unknown platforms
+		:returns:
+			A list of the serial ports available on the system
+	"""
+	if sys.platform.startswith('win'):
+		ports = ['COM%s' % (i + 1) for i in range(256)]
+	elif sys.platform.startswith('linux') or sys.platform.startswith('cygwin'):
+		# this excludes your current terminal "/dev/tty"
+		ports = glob.glob('/dev/tty[A-Za-z]*')
+	elif sys.platform.startswith('darwin'):
+		ports = glob.glob('/dev/tty.*')
+	else:
+		raise EnvironmentError('Unsupported platform')
 
-    while True:
-        try:
-            arduinoStringRaw = arduinoSerialDevice.readline().decode('utf-8')
-            arduino_data = json.loads(arduinoStringRaw)
-            sensorObj.temperature = arduino_data['Temperature']
-            sensorObj.humidity = arduino_data['Humidity']
-            print(sensorObj)
-        except KeyboardInterrupt:
-            print('Exiting ... ')
-            sys.exit(0)
+	result = []
+	for port in ports:
+		try:
+			s = serial.Serial(port)
+			s.close()
+			result.append(port)
+		except (OSError, serial.SerialException):
+			pass
+	return result
 
+
+def listen_sensor_data(sensorObj):
+	serial_ports = list_serial_ports()
+	if len(serial_ports) == 0:
+		sys.exit('No Serial Ports found. Exiting')
+	
+	for index, port in enumerate(serial_ports):
+		print(f'{index}. {port}')
+	
+	selection = int(input('Please select the COM port corresponding to the attached microcontroller device. (Eg: 1, 2, 3 etc)\n'))
+	baudrate = int(input('Please input the baudrate of the attached device\n'))
+
+	serial_device = serial.Serial(serial_ports[selection], baudrate)
+	raw_string = 'Error, No Data'
+
+	while True:
+		try:
+			raw_string = serial_device.readline().decode('utf-8')
+			data_point = json.loads(raw_string)
+			sensorObj.temperature = data_point['Temperature']
+			sensorObj.humidity = data_point['Humidity']
+			print(sensorObj)
+		except KeyboardInterrupt:
+			print('Exiting ... ')
+			sys.exit(0)
 
 sensor1 = SensorData()
-
-
 def start_serial_communication_thread():
-    arduino_thread = threading.Thread(target=listenSensorData, args=(sensor1,))
-    arduino_thread.start()
+	
+	arduino_thread = threading.Thread(target=listen_sensor_data, args=(sensor1,))
+	arduino_thread.start()
 
 
 if __name__ == '__main__':
-    sensor1 = SensorData()
-    listenSensorData(sensor1)
-    pass
+	sensor1 = SensorData()
+	listen_sensor_data(sensor1)
+	pass
