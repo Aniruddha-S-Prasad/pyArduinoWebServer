b01c94e92bc8471524fa9d3595598e62e66d20db
Aniruddha S Prasad <aniruddha.asp12@gmail.com>
2021-12-24 01:52:19 +0100

    Datalogger changed to be polling slower and saving every ten minutes

--- data_logger.py
+++ data_logger.py
@@ -1,16 +1,16 @@
-import sys, time, glob
-import json, csv
+import sys, time, glob, os.path
+from datetime import date, timedelta
+import pandas as pd
+import json
 import serial
-
-database_filename = 'databases/weather_data.db'
 
 
 def listSerialPorts() -> list:
 	""" Lists serial port names
 
-		:raises EnvironmentError:
+		raises: EnvironmentError
 			On unsupported or unknown platforms
-		:returns:
+		returns:
 			A list of the serial ports available on the system
 	"""
 	if sys.platform.startswith('win'):
@@ -24,15 +24,31 @@ def listSerialPorts() -> list:
 	result = []
 	for port in ports:
 		try:
-			s = serial.Serial(port)
-			s.close()
+			serial_handle = serial.Serial(port)
+			serial_handle.close()
 			result.append(port)
 		except (OSError, serial.SerialException):
 			pass
 	return result
 
-	
+def create_new_database_file():
+	weather_data_keys = ['Temperature', 'Humidity', 'HeatIndex', 'Timestamp']
+
+	file_date_string = time.strftime('%Y-%m-%d', time.localtime())
+	database_filename = 'databases/room_weather_' + file_date_string + '.csv'
+
+	if not os.path.isfile(database_filename):
+		weather_dataframe = pd.DataFrame(columns=weather_data_keys)
+		weather_dataframe.to_csv(database_filename, mode='w', index=False)
+		print('Created new file on ' + file_date_string)
+
+
 def logSensorData():
+	start_day = date.today()
+
+	current_day = date.today()
+	previous_day = date.today()
+
 	serial_ports = listSerialPorts()
 	if len(serial_ports) == 0:
 		sys.exit('No Serial Ports found. Exiting')
@@ -40,40 +56,57 @@ def logSensorData():
 	for index, port in enumerate(serial_ports):
 		print(f'{index}. {port}')
 	
-	selection = int(input('Please select the COM port corresponding to the attached microcontroller device. (Eg: 1, 2, 3 etc)\n'))
-
+	selection = int(input('Please select the serial port corresponding to the attached microcontroller device. (Eg: 1, 2, 3 etc)\n'))
 
 	serial_device = serial.Serial(serial_ports[selection], 115200)
-	arduino_data = {}
-	arduino_data_keys = ['Timestamp', 'Temperature', 'Humidity', 'HeatIndex']
-	counter = 0
 
-	file_time_string = time.strftime('%Y-%m-%d_%Hh%Mm%Ss', time.localtime())
-	with open('databases/room_climate_' + file_time_string + '.csv', 'w', newline='') as data_file:
-		data_writer = csv.DictWriter(data_file, fieldnames=arduino_data_keys)
-		data_writer.writeheader()
+	file_date_string = time.strftime('%Y-%m-%d', time.localtime())
+	database_filename = 'databases/room_weather_' + file_date_string + '.csv'
+
+	create_new_database_file()
+
+	while True:
+		current_day = date.today()
 		
-		while True:
-			try:
-				raw_received_string = serial_device.readline().decode('utf-8')
-				current_time_string = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime())
+		if current_day == previous_day:
+			pass
+		elif (current_day - previous_day) == timedelta(days=1):
+			create_new_database_file()
+			previous_day = date.today()
 
-				try:
-					arduino_data = json.loads(raw_received_string)
-					arduino_data['Timestamp'] = int(time.time())
-					data_writer.writerow(arduino_data)
-				except json.JSONDecodeError:
-					print(f'JSON Error at {current_time_string}, probably a serial communication error')
-					continue
-				
-				print(f'At {current_time_string}, the temperature was {arduino_data["Temperature"]} and the humudity was {arduino_data["Humidity"]}')
-				
-				counter += 1
-				if counter % 100 == 0:
-					data_file.flush()
+		elif (current_day - previous_day) > timedelta(days=1):
+			raise RuntimeError('Seems that one day or more has been skipped!!')
 
-			except KeyboardInterrupt:
-				break
+		elif (current_day - previous_day) < timedelta(days=0):
+			raise RuntimeError('How is this even possible?!, current day is before start day?')
+
+		try:
+			weather_data = []
+			for ctr in range(40):
+
+					serial_device.flushInput()
+					raw_received_string = serial_device.readline().decode('utf-8')
+					current_time_string = time.strftime('%d-%m-%Y %H:%M:%S', time.localtime())
+
+					try:
+						weather_datapoint = json.loads(raw_received_string)
+						weather_datapoint['Timestamp'] = int(time.time())
+						weather_data.append(weather_datapoint)
+					except json.JSONDecodeError:
+						print(f'JSON Error at {current_time_string}, probably a serial communication error')
+						continue
+					
+					print(f'At {current_time_string}, the temperature was {weather_datapoint["Temperature"]} and the humudity was {weather_datapoint["Humidity"]}')
+					time.sleep(15)
+					
+			weather_dataframe = pd.DataFrame(weather_data)
+			weather_dataframe.to_csv(database_filename, mode='a', header=False, index=False)
+			
+			weather_dataframe.drop(weather_dataframe.index, inplace=True)
+		except KeyboardInterrupt:
+			weather_dataframe = pd.DataFrame(weather_data)
+			weather_dataframe.to_csv(database_filename, mode='a', header=False, index=False)
+			break
 		
 	print('Exiting ... ')
 	return
