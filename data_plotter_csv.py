5cfe7ae6db164850d81284de6291d378362350d9
Aniruddha S Prasad <aniruddha.asp12@gmail.com>
2021-12-24 11:45:51 +0100

    Fixed time localization issues

--- data_plotter_csv.py
+++ data_plotter_csv.py
@@ -1,17 +1,18 @@
 import pandas as pd
+from pytz import timezone
 from matplotlib import pyplot as plt
 from matplotlib import dates as mdates
 
 def main():
-	weather_data = pd.read_csv('databases/room_weather_2021-12-23.csv')
+	weather_data = pd.read_csv('databases/room_weather_2021-12-24.csv')
 
-	timestamps = pd.to_datetime(weather_data['Timestamp'], unit='s')
+	weather_data['Timestamp'] = pd.to_datetime(weather_data['Timestamp'], unit='s')
 
 	fig, ax = plt.subplots(figsize=(10,7))
-	ax.plot_date(timestamps, weather_data['Temperature'], '-b', label='Temperature')
+	ax.plot_date(weather_data.Timestamp, weather_data.Temperature, '-b', label='Temperature')
 
-	timestamp_locator = mdates.AutoDateLocator(minticks=7, maxticks=10)
-	timestamp_format = mdates.ConciseDateFormatter(timestamp_locator)
+	timestamp_locator = mdates.AutoDateLocator(minticks=4, maxticks=10)
+	timestamp_format = mdates.ConciseDateFormatter(timestamp_locator, tz=timezone('CET'))
 
 	ax.xaxis.set_major_locator(timestamp_locator)
 	ax.xaxis.set_major_formatter(timestamp_format)
